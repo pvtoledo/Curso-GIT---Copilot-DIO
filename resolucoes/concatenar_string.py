5a998bfec8b0f342908c7b58530b136de7379b1a
Paulo Vinicius Toledo <50490992+pvtoledo@users.noreply.github.com>
2024-04-13 17:30:00 -0300

    Create concatenar_string.py

---
+++ resolucoes/concatenar_string.py
@@ -0,0 +1,9 @@
+# Recebendo os dados do usuário
+dado1 = input("Digite o primeiro dado: ")
+dado2 = input("Digite o segundo dado: ")
+
+# Concatenando os dados em uma única string
+concatenado = dado1 + dado2
+
+# Imprimindo a string concatenada
+print("String concatenada:", concatenado)
