bfeadfee5a1917a09c2231057c26477a648f57ba
Paulo Vinicius Toledo <50490992+pvtoledo@users.noreply.github.com>
2024-04-13 17:55:10 -0300

    Create media_notas.py

---
+++ resolucoes/media_notas.py
@@ -0,0 +1,10 @@
+# Solicitando as três notas como entrada
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+# Calculando a média das notas
+media = (nota1 + nota2 + nota3) / 3
+
+# Exibindo a média das notas
+print("A média das notas é:", media)
